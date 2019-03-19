6e9c571a8497becedb002591d47e0d59b67e7784
yuyi <38847066+ys558@users.noreply.github.com>
2019-03-19 11:46:43 +0800

    Create 1.2.1_fluentPython.py

---
+++ 1.2.1_fluentPython.py
@@ -0,0 +1,39 @@
+from math import hypot
+
+# 自定义向量类：
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)'%(self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x*scalar, self.y*scalar)
+
+    def __pow__(self, index):
+        return Vector(self.x**index, self.y**index)
+
+    
+v1 = Vector(2,4)
+v2 = Vector(2,3)
+print(v1+v2)
+# Vector(4, 7)
+print(abs(v1))
+# 4.47213595499958
+print(v2*3)
+# Vector(6, 9)
+print(v2**3)
+# Vector(8, 27)
